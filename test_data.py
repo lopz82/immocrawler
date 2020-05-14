0749eef73bde18923edd37321b9f52a5b1503e06
Jorge Lopez Guzman <jorge.lopez.guzman@outlook.es>
2020-05-14 21:43:36 +0200

    Test data

---
+++ test_data.py
@@ -0,0 +1,29 @@
+R1 = {'title': 'Ankündigung - Top 3 ZKB in der Bruchfeldstraße 29 - gegenüber von Rewe',
+      'address': ' Bruchfeldstr. 29,', 'zip-city-neighborhood': '60528 Frankfurt am Main, Niederrad',
+      'type': ' Etagenwohnung ', 'floor': ' 3 von 5 ', 'aprox. surface': ' 105 m² ', 'available from': ' 01.08.2020 ',
+      'rooms': ' 3 ', 'sleeping rooms': ' 1 ', 'restrooms': ' 1 ', 'pets': ' Nach Vereinbarung ',
+      'garage': ' 1 Tiefgaragen-Stellplatz ', 'rent': ' 1.350 € ', 'side costs': ' + 250 € ',
+      'heating': '  in Nebenkosten enthalten ', 'deposit': '4.585,00 ', 'rent for garage': ' 100 € ',
+      'construction year': '2020', 'status': ' Neuwertig ', 'heating type': ' Zentralheizung ',
+      'description': 'Dieses Wohn- und Geschäftshaus besteht aus insgesamt 14 Einheiten mit 10 Wohnungen und 4 Gewerbeeinheiten (Restaurant, Pizza Hut, Main Burger, Friseur). Gegenüberliegend sind die Nahversorger Rewe, Bäckerei u.v.m.\nIn dieser großzügigen 2-Zimmerwohnung ist der Wohn- Essbereich mit einer integrierten, exklusiven Einbauküche. Das Schlafzimmer ist geräumig, das Badezimmer ist ausgestattet mit einer Ebenerdigen Dusche und einer Badewanne. Alle Räume sind hochwertig ausgestattet und haben Fußbodenheizung, genießen Sie modernen Wohnkomfort fast mitten in Großstadt.',
+      'equipment': '- elegantes Bad mit Wanne und Dusche Echtglasduschkabine\n- Waschmaschinenanschluss im Bad\n- Gäste-WC\n- hochwertige Einbauküche - edle Parkettböden\n- edle Feinsteinzeugboden - Fußbodenheizung\n- Aufzug\n- separater Keller\n- Fahrradkeller\n- Videosprechanlage\n- Kabelanschluss\n- modernster Telefon- und Internetanschluss'}
+
+R2 = {'title': 'Lichtdurchflutete Penthousewohnung mit großer Dachterrasse', 'address': ' Waldschulstraße 68a,',
+      'zip-city-neighborhood': '65933 Frankfurt, Griesheim', 'type': ' Penthouse ', 'floor': ' 4 von 4 ',
+      'aprox. surface': ' 107 m² ', 'available from': ' sofort ', 'rooms': ' 4 ', 'sleeping rooms': ' 3 ',
+      'restrooms': ' 2 ', 'pets': ' Nach Vereinbarung ', 'garage': ' 1 Tiefgaragen-Stellplatz ', 'rent': ' 1.349,77 € ',
+      'side costs': ' + 266,80 € ', 'heating': '  in Nebenkosten enthalten ', 'deposit': '3 Monatskaltmieten ',
+      'rent for garage': ' 80 € ', 'construction year': '2017', 'status': ' Neuwertig ',
+      'heating type': ' Blockheizkraftwerke ',
+      'description': 'Unsere wunderschöne 4-Zimmer Penthousewohnung befindet sich in einem Neubaugebäudekomplex, der im Jahr 2017 fertiggestellt wurde. Der Komplex besteht aus 3 Mehrfamilienhäusern, welche von einem offen gestalteten Innenhof mit großzügiger Grünfläche umgeben werden - eine Oase im Großstadtjungel.',
+      'equipment': 'Das Highlight der wunderschönen Penthouse- Wohnung (4 Zimmer) ist die große Dachterrasse mit viel Platz zum Entspannen. \n\nZudem verfügt die Wohnung über ein Badezimmer mit Badewanne, sowie einer Dusche. Weiterhin ist ein separates Gäste-WC mit Waschmaschinenanschluss vorhanden. Zur Wohnung gehört ein Stellplatz in der objekteigenen Tiefgarage (dieser wurde bereits in die Gesamtmiete integriert).\n\nNeben der gehobenen Ausstattung, inklusive einer neuwertigen Einbauküche mit hochwertigen Elektrogeräten gehört außerdem ein geräumiges Kellerabteil zur Wohnung.'}
+
+R3 = {'title': 'Ankündigung - Top 3 ZKB in der Bruchfeldstraße 29 - gegenüber von Rewe',
+      'address': ' Bruchfeldstr. 29,', 'zip-city-neighborhood': '60528 Frankfurt am Main, Niederrad',
+      'type': ' Etagenwohnung ', 'floor': ' 3 von 5 ', 'aprox. surface': ' 105 m² ', 'available from': ' 01.08.2020 ',
+      'rooms': ' 3 ', 'sleeping rooms': ' 1 ', 'restrooms': ' 1 ', 'pets': ' Nach Vereinbarung ',
+      'garage': ' 1 Tiefgaragen-Stellplatz ', 'rent': ' 1.350 € ', 'side costs': ' + 250 € ',
+      'heating': '  in Nebenkosten enthalten ', 'deposit': '4.585,00 ', 'rent for garage': ' 100 € ',
+      'construction year': '2020', 'status': ' Neuwertig ', 'heating type': ' Zentralheizung ',
+      'description': 'Dieses Wohn- und Geschäftshaus besteht aus insgesamt 14 Einheiten mit 10 Wohnungen und 4 Gewerbeeinheiten (Restaurant, Pizza Hut, Main Burger, Friseur). Gegenüberliegend sind die Nahversorger Rewe, Bäckerei u.v.m.\nIn dieser großzügigen 2-Zimmerwohnung ist der Wohn- Essbereich mit einer integrierten, exklusiven Einbauküche. Das Schlafzimmer ist geräumig, das Badezimmer ist ausgestattet mit einer Ebenerdigen Dusche und einer Badewanne. Alle Räume sind hochwertig ausgestattet und haben Fußbodenheizung, genießen Sie modernen Wohnkomfort fast mitten in Großstadt.',
+      'equipment': '- elegantes Bad mit Wanne und Dusche Echtglasduschkabine\n- Waschmaschinenanschluss im Bad\n- Gäste-WC\n- hochwertige Einbauküche - edle Parkettböden\n- edle Feinsteinzeugboden - Fußbodenheizung\n- Aufzug\n- separater Keller\n- Fahrradkeller\n- Videosprechanlage\n- Kabelanschluss\n- modernster Telefon- und Internetanschluss'}
