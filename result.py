ae5f96fddd9199f5ca98e4ded3bb8c291ca15d8a
Jorge Lopez Guzman <jorge.lopez.guzman@outlook.es>
2020-05-14 20:23:42 +0200

    SHA256 calculation based on extracted data. Now it is performed before adding geodata and date

--- result.py
+++ result.py
@@ -7,14 +7,17 @@ class Result:
     def __init__(self, url, **kwargs):
         self.__dict__.update(**kwargs)
         self.url = url
-        self.date = datetime.now()
+        # Calculating sha256 before we add the date (variable) and latitude
+        # and longitude (maybe variable, dependant on Here)
+        self.sha256 = sha256(str(self.__dict__).encode()).hexdigest()
         self.latitude = None
         self.longitude = None
         self.geolocate()
-        self.sha256 = sha256(str(self.__dict__).encode()).hexdigest()
+        self.date = datetime.now()
 
     def geolocate(self):
-        assert(getattr(self, "address"))
+        # As we just update __dict__ with the provided args, we should make sure
+        # that an address attribute exists
         if self.address:
             self.latitude, self.longitude = geolocation(self.address)
 
