50b3cb4a3e4348c19d9b0220e39f6691701d6ace
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-15 15:56:24 +0200

    Added message broker module

--- messaging.py
+++ messaging.py
@@ -1,22 +1,26 @@
-from typing import Iterable
-
-from azure.storage.queue import QueueClient
+from azure.storage.queue import QueueClient, QueueMessage
 
 from config import CONFIG
+from utils import get_logger
 
 CONNECTION_STRING = CONFIG["azure_queue"]["connection_string"]
 QUEUE_NAME = "frankfurt"
 
+log = get_logger(__name__)
 
-class QueueClient:
+
+class AzureStorageQueueClient:
 
     def __init__(self):
         self.client = QueueClient.from_connection_string(CONNECTION_STRING, QUEUE_NAME)
 
-    def send_meesage(self, message: str) -> None:
+    def send_message(self, message: str) -> None:
         self.client.send_message(message)
 
-    def receive_messages_content(self) -> Iterable[str]:
+    def receive_message(self) -> QueueMessage:
         messages = self.client.receive_messages()
         for msg in messages:
-            return msg.content
+            return msg
+
+    def delete_message(self, message: QueueMessage):
+        self.client.delete_message(message)
