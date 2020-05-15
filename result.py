f73347efb5bcfe51db9ca0b79e195c92ca9b5036
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-15 15:51:02 +0200

    Fixed two bugs in geolocate module: - sha256 generated from extracted data, not including geodata, date or url. All non deterministic data should be excluded. - geolocate now uses address and zip code.

--- result.py
+++ result.py
@@ -1,22 +1,28 @@
 from datetime import datetime
 from hashlib import sha256
+
 from geo import geolocation
 
 
 class Result:
     def __init__(self, url, **kwargs):
         self.__dict__.update(**kwargs)
+        self.sha256 = self.generate_sha256()
         self.url = url
         self.date = datetime.now()
         self.latitude = None
         self.longitude = None
         self.geolocate()
+
+    def generate_sha256(self):
+
         self.sha256 = sha256(str(self.__dict__).encode()).hexdigest()
 
     def geolocate(self):
-        assert(getattr(self, "address"))
-        if self.address:
-            self.latitude, self.longitude = geolocation(self.address)
+        assert (getattr(self, "address"))
+        assert (getattr(self, "zip_city_neighborhood"))
+        if self.address and self.zip_city_neighborhood:
+            self.latitude, self.longitude = geolocation(f"{self.address} {self.zip_city_neighborhood}")
 
     def __repr__(self):
         return f"{__class__.__name__} {self.url}"
