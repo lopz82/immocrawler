e26c49a15627dc183e056cb6af5e615abeb9b035
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-15 15:57:56 +0200

    Refactored tasks module

--- tasks.py
+++ tasks.py
@@ -1,46 +1,44 @@
-import logging
-import sys
+from typing import Type, TypeVar
 
 import requests
 from bs4 import BeautifulSoup
 
-from config import CRITERIA, HEADERS
+from config import CRITERIA, HEADERS, BASE_URL
 from result import Result
+from utils import get_logger
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-log.addHandler(handler)
+log = get_logger(__name__)
 
 
-class SpecialSearchTask:
-    def __init__(self, url):
-        self.url = url
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.url})"
-
-    def run(self):
-        pass
-
-
-class StandardSearchTask:
-    def __init__(self, url):
+class SearchTask:
+    def __init__(self, url: str) -> None:
         self.url = url
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.url})"
 
+
+class SpecialSearchTask(SearchTask):
+    def __init__(self, url: str) -> None:
+        super(SpecialSearchTask, self).__init__(url)
+
+    def run(self):
+        pass
+
+
+class StandardSearchTask(SearchTask):
+    def __init__(self, url: str) -> None:
+        super(StandardSearchTask, self).__init__(BASE_URL + url)
+
     def run(self) -> Result:
         data = {}
-        soup = self.get_html()
+        soup = self._get_html()
         image_source = soup.select("div.first-gallery-picture-container img")
         if len(image_source) == 1:
             data["image"] = image_source[0].get("src", "Not found")
         else:
-            log.error(f"Error extracting image with div.first-gallery-picture-container img. Expecting one result, got {len(image_source)}.")
+            log.error(
+                f"Error extracting image with div.first-gallery-picture-container img. Expecting one result, got {len(image_source)}.")
         for section, CSS_selector in CRITERIA.items():
             log.info(f"Extracting {section} with {CSS_selector} selector.")
             item = soup.select(CSS_selector)
@@ -54,8 +52,24 @@ class StandardSearchTask:
         result = Result(self.url, **data)
         return result
 
-    def get_html(self) -> BeautifulSoup:
+    def _get_html(self) -> BeautifulSoup:
         log.info(f"Crawling {self.url}.")
         html = requests.get(self.url, headers=HEADERS).content
         soup = BeautifulSoup(html, "html.parser")
         return soup
+
+
+Task = TypeVar("Task", bound=SearchTask)
+
+
+class SearchTaskFactory:
+    @classmethod
+    def new_task(cls, url: str) -> Type[Task]:
+        if cls.is_special_task(url):
+            return SpecialSearchTask(url)
+        else:
+            return StandardSearchTask(url)
+
+    @staticmethod
+    def is_special_task(url: str) -> bool:
+        return url.startswith("https://")
