4452f3b8fdc2e3575530eb0eb5c5a23f2ad98e34
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-15 15:47:13 +0200

    Added logging

--- geo.py
+++ geo.py
@@ -3,13 +3,15 @@ from typing import Tuple
 import requests
 
 from config import CONFIG
+from utils import get_logger
 
 URL = "https://geocode.search.hereapi.com/v1/geocode"
 API_KEY = CONFIG["here"]["api_key"]
+
+log = get_logger(__name__)
 
 
 def geolocation(address: str) -> Tuple[float, float]:
     params = {"apikey": API_KEY, "q": address}
     data = requests.get(URL, params).json()
     return float(data['items'][0]['position']['lat']), float(data['items'][0]['position']['lng'])
-
