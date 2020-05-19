dbbad7afabc2f529977b521bebe9b4c7c99a5bba
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-19 19:57:09 +0200

    Results objects now extract zip code, city and neighborhood

--- immocrawler/result.py
+++ immocrawler/result.py
@@ -1,22 +1,28 @@
+import hashlib
 from datetime import datetime
-from hashlib import sha256
 
 from immocrawler.geo import geolocation
+from immocrawler.utils import extract_zip_code, extract_city, extract_neighborhood
 
 
 class Result:
     def __init__(self, url, **kwargs):
         self.__dict__.update(**kwargs)
-        self.sha256 = self.generate_sha256()
+        self.sha256 = None
+        self.generate_sha256()
+        if self.zip_city_neighborhood:
+            self.zip_code = extract_zip_code(self.zip_city_neighborhood)
+            self.city = extract_city(self.zip_city_neighborhood)
+            self.neighborhood = extract_neighborhood(self.zip_city_neighborhood)
         self.url = url
         self.date = datetime.now()
         self.latitude = None
         self.longitude = None
         self.geolocate()
 
-    def generate_sha256(self):
 
-        self.sha256 = sha256(str(self.__dict__).encode()).hexdigest()
+    def generate_sha256(self):
+        self.sha256 = hashlib.sha256(str(self.__dict__).encode()).hexdigest()
 
     def geolocate(self):
         assert (getattr(self, "address"))
@@ -25,11 +31,8 @@ class Result:
             self.latitude, self.longitude = geolocation(f"{self.address} {self.zip_city_neighborhood}")
 
     def __repr__(self):
-        return f"{__class__.__name__} {self.url}"
-
-    def __str__(self):
-        return str(self.__dict__)
+        return f"{__class__.__name__}({self.url})"
 
     @property
-    def data(self):
+    def data(self):  # pragma: no cover
         return self.__dict__
--- immocrawler/utils.py
+++ immocrawler/utils.py
@@ -20,3 +20,16 @@ def random_time(wait) -> int:
     time = random.randint(*wait)
     log.info(f"Generated random time: {time} s.")
     return time
+
+
+def extract_city(s: str) -> str:
+    s = s.rsplit(",", maxsplit=1)[0]
+    return s.split(" ", maxsplit=1)[1]
+
+
+def extract_neighborhood(s: str) -> str:
+    return s.rsplit(",")[-1].strip()
+
+
+def extract_zip_code(s: str) -> int:
+    return int(s.split(" ")[0])
