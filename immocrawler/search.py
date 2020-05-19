83cbe19bb1cba18dcd8463d8f42aa5a3248ccae0
lopz82 <jorge.lopez.guzman@gmail.com>
2020-05-19 20:01:23 +0200

    Searching logic isolated from HTTP requests logic

--- immocrawler/search.py
+++ immocrawler/search.py
@@ -2,11 +2,11 @@ import random
 from time import sleep
 from typing import Tuple, List
 
-import requests
 from bs4 import BeautifulSoup
 
-from immocrawler.config import BASE_URL, HEADERS, PAGE_NUMBER_SLUG, MAIN_LINK
+from immocrawler.config import BASE_URL, PAGE_NUMBER_SLUG, MAIN_LINK
 from immocrawler.utils import get_logger, random_time
+from immocrawler.webdrivers import RequestsDriver
 
 log = get_logger(__name__)
 
@@ -14,11 +14,12 @@ log = get_logger(__name__)
 class ImmobilienScoutSearch:
     def __init__(self, wait: Tuple[int] = None, rooms: int = None, price: float = None,
                  living_space: float = None) -> None:
+        self.driver = RequestsDriver()
         self.rooms = rooms
         self.price = price
         self.living_space = living_space
         self.url = BASE_URL + self.add_params_to_url()
-        self.soup = ImmobilienScoutSearch.crawl(self.url)
+        self.soup = self.crawl(self.url)
         self.n_pages = None
         self.wait = (5, 10) or tuple(wait)  # Long wait to fake human behaviour
         self.target_links = []
@@ -34,17 +35,8 @@ class ImmobilienScoutSearch:
             params.append(f"livingspace={self.living_space}")
         return "/Suche/de/hessen/frankfurt-am-main/wohnung-mieten?" + "&".join(params)
 
-    @staticmethod
-    def crawl(url) -> BeautifulSoup:
-        log.info(f"Crawling {url}")
-        return BeautifulSoup(ImmobilienScoutSearch.request_get(url), "html.parser")
-
-    @staticmethod
-    def request_get(url: str) -> bytes:
-        try:
-            return requests.get(url, headers=HEADERS).content
-        except requests.exceptions.RequestException as ex:
-            log.error(f"Error after GET request to {url}: {ex}")
+    def crawl(self, url) -> BeautifulSoup:
+        return BeautifulSoup(self.driver.html_from(url), "html.parser")
 
     def get_pages_num(self) -> None:
         self.n_pages = len(self.soup.select("div#pageSelection select.select option"))
@@ -72,10 +64,10 @@ class ImmobilienScoutSearch:
     def get_target_links_from(self, url: str) -> None:
         sleep(random_time(self.wait))
         log.info(f"Crawling {url}.")
-        html = ImmobilienScoutSearch.crawl(url)
+        html = self.crawl(url)
         log.info(f"Extracting links from CSS selector.")
-        soup = html.select(MAIN_LINK)
-        links = [match["href"] for match in soup]
+        matches = html.select(MAIN_LINK)
+        links = [match["href"] for match in matches]
         log.info(f"{len(links)} links found.")
         self.target_links.extend(links)
         log.info(f"Added {len(links)} new links")
